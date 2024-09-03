d567b0a6d607fade04cbcc1e2e5ea00ee5d5f9c2
Prajwal Neral <156423013+Prajwalneral@users.noreply.github.com>
2024-09-03 23:18:16 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+    using namespace std;
+    int main()
+    {
+        int =num1,num2;
+
+        cout<<"enter first no = ";
+        cin>>num1;
+
+        cout<<"enter second no =";
+        cin>>num2;
+
+        cout<<"add = " <<num1+num2;
+
+
+       return 0; 
+    }
